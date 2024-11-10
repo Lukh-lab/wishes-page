eed66df8eb09461f7bb25acb2714a5a5744c281f
Lukh-lab <lukh18hakim@gmail.com.com>
2024-11-10 17:27:53 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Particles from 'react-particles';
 import Confetti from 'react-confetti';
 import Gallery from './Gallery';
